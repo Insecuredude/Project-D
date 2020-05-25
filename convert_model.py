04dfd1edada933718432e6dea88bf4cce507e224
guusjoppe <guusjoppe@gmail.com>
2020-05-25 11:12:45 +0200

    Convert Model

--- convert_model.py
+++ convert_model.py
@@ -5,3 +5,7 @@ save_path = 'trash_recognizer_model'
 # Convert the model.
 converter = tf.lite.TFLiteConverter.from_saved_model(save_path)
 tflite_model = converter.convert()
+
+# Writes the tflite model to the disk to be used as a file
+open("model.tflite", "wb").write(tflite_model)
+
