65ea9666ac8b5ab10e095e7dd33373f68c5f1dac
Hicham <hichagzanay@gmail.com>
2020-05-25 21:33:07 +0200

    camera preview
    De app geeft een camera preview. de knop om fotos op te slaan werkt helaas niet. bugs met de permissions.

--- Trash_Recognizer/app/src/main/java/com/example/trash_recognizer/MainActivity.java
+++ Trash_Recognizer/app/src/main/java/com/example/trash_recognizer/MainActivity.java
@@ -1,14 +1,135 @@
 package com.example.trash_recognizer;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.sql.Time;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements SurfaceHolder.Callback {
+
+    private Button button;
+    private SurfaceView surfaceView;
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+    private android.hardware.Camera.PictureCallback pictureCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 100);
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        surfaceView = findViewById(R.id.surfaceView);
+        button = findViewById(R.id.button);
+
+        surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                camera.takePicture(null, null, pictureCallback);
+            }
+        });
+
+        pictureCallback = new android.hardware.Camera.PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] bytes, android.hardware.Camera camera) {
+                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                Bitmap cbmp = Bitmap.createBitmap(bmp, 0, 0, bmp.getWidth(), bmp.getHeight(), null, true);
+
+                String pathFileName = currentDateFormat();
+                storePhotoToStorage(cbmp, pathFileName);
+
+                Toast.makeText(getApplicationContext(), "Done!", Toast.LENGTH_LONG).show();
+
+                MainActivity.this.camera.startPreview();
+
+            }
+        };
+    }
+
+    private void storePhotoToStorage(Bitmap cbmp, String pathFileName) {
+        File outputFile = new File(Environment.getExternalStorageDirectory(), "/DCIM/Camera/IMG_"+pathFileName+".jpg");
+
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+            cbmp.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String currentDateFormat() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HH_mm_ss");
+        String currentTime = dateFormat.format(new Date());
+        return  currentTime;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        try {
+            camera = Camera.open();
+        } catch (Exception e) {
+
+        }
+
+        Camera.Parameters parameters;
+        parameters = camera.getParameters();
+        parameters.setPreviewFrameRate(30);
+        parameters.setPreviewSize(352, 288);
+        camera.setParameters(parameters);
+        camera.setDisplayOrientation(90);
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+        camera = null;
     }
 }
